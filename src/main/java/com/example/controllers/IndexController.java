8b7bd40c6ec792e48c269e90d1cebcf006c648f8
TexasVoloshin <!!2019!!Texas>
2020-04-03 21:25:49 +0200

    showIndexPage added

--- src/main/java/com/example/controllers/IndexController.java
+++ src/main/java/com/example/controllers/IndexController.java
@@ -1,4 +1,15 @@
 package com.example.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IndexController {
+
+    @GetMapping("/")
+
+    public String showIndexPage(){
+        return "index";
+    }
 }
