383cc5d807cf87e722f98f400cc6a9813ae5a237
TexasVoloshin <!!2019!!Texas>
2020-04-05 01:26:59 +0200

    read index.html , doesnt read header and footer

--- src/main/java/com/example/controllers/IndexController.java
+++ src/main/java/com/example/controllers/IndexController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+
 public class IndexController {
 
     @GetMapping("/")
