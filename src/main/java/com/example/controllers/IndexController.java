43ccec4c77b95404f9530188e446d652ffafc06a
TexasVoloshin <!!2019!!Texas>
2020-04-04 20:52:24 +0200

    added additional maven dependencies

--- src/main/java/com/example/controllers/IndexController.java
+++ src/main/java/com/example/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package com.example.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@ResponseBody
 public class IndexController {
 
     @GetMapping("/")
