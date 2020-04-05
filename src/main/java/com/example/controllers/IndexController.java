4c0d6b9a17805fe54e40a8cc4ea2f20b608aede3
TexasVoloshin <!!2019!!Texas>
2020-04-05 21:24:08 +0200

    Templates are working and displaying correctly !

--- src/main/java/com/example/controllers/IndexController.java
+++ src/main/java/com/example/controllers/IndexController.java
@@ -2,16 +2,16 @@ package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-
+@Controller
 public class IndexController {
 
-    @GetMapping("/")
 
+
+    @GetMapping("/")
     public String showIndexPage(){
+        ModelAndView modelAndView = new ModelAndView("index");
         return "index";
     }
 }
