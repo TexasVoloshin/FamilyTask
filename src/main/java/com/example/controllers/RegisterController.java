f62d2ec540688c6015322d15833b3b3f2f9826ea
TexasVoloshin <!!2019!!Texas>
2020-04-05 23:51:00 +0200

    register form added and working

---
+++ src/main/java/com/example/controllers/RegisterController.java
@@ -0,0 +1,18 @@
+package com.example.controllers;
+
+import com.example.entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RegisterController {
+    @GetMapping("/register")
+    public String registerForm(Model model){
+        model.addAttribute("user", new User());
+        return "views/registerform";
+
+
+    }
+}
